e9d17214a431a90c9ec78fc912781048907f42b2
Sinri Edogawa <e.joshua.s.e@gmail.com>
2025-11-07 14:58:18 +0800

    Add JsonUtils for JSON manipulation utilities and sorting methods
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

---
+++ src/main/java/io/github/sinri/keel/utils/json/JsonUtils.java
@@ -0,0 +1,154 @@
+package io.github.sinri.keel.utils.json;
+
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class JsonUtils {
+    /**
+     * @since 2.4
+     */
+    @Nonnull
+    private JsonArray getSortedJsonArray(@Nonnull JsonArray array) {
+        List<Object> list = new ArrayList<>();
+        array.forEach(list::add);
+        list.sort(Comparator.comparing(Object::toString));
+        return new JsonArray(list);
+    }
+
+    /**
+     * @since 2.4
+     */
+    @Nonnull
+    public String getJsonForArrayWhoseItemsSorted(@Nonnull JsonArray array) {
+        return getSortedJsonArray(array).toString();
+    }
+
+    /**
+     * @since 2.4
+     */
+    @Nonnull
+    private JsonObject getSortedJsonObject(@Nonnull JsonObject object) {
+        JsonObject result = new JsonObject();
+        List<String> keyList = new ArrayList<>(object.getMap().keySet());
+        keyList.sort(Comparator.naturalOrder());
+        keyList.forEach(key -> {
+            Object value = object.getValue(key);
+            if (value instanceof JsonObject) {
+                result.put(key, getSortedJsonObject((JsonObject) value));
+            } else if (value instanceof JsonArray) {
+                result.put(key, getSortedJsonArray((JsonArray) value));
+            } else {
+                result.put(key, value);
+            }
+        });
+        return result;
+    }
+
+    /**
+     * @since 2.4
+     */
+    @Nonnull
+    public String getJsonForObjectWhoseItemKeysSorted(@Nonnull JsonObject object) {
+        return getSortedJsonObject(object).toString();
+    }
+
+    public void filterStackTrace(
+            @Nullable StackTraceElement[] stackTrace,
+            @Nonnull Set<String> ignorableStackPackageSet,
+            @Nonnull BiConsumer<String, Integer> ignoredStackTraceItemsConsumer,
+            @Nonnull Consumer<StackTraceElement> stackTraceItemConsumer
+    ) {
+        if (stackTrace != null) {
+            String ignoringClassPackage = null;
+            int ignoringCount = 0;
+            for (StackTraceElement stackTranceItem : stackTrace) {
+                String className = stackTranceItem.getClassName();
+                String matchedClassPackage = null;
+                for (var cp : ignorableStackPackageSet) {
+                    if (className.startsWith(cp)) {
+                        matchedClassPackage = cp;
+                        break;
+                    }
+                }
+                if (matchedClassPackage == null) {
+                    if (ignoringCount > 0) {
+                        ignoredStackTraceItemsConsumer.accept(ignoringClassPackage, ignoringCount);
+                        ignoringClassPackage = null;
+                        ignoringCount = 0;
+                    }
+
+                    stackTraceItemConsumer.accept(stackTranceItem);
+                } else {
+                    if (ignoringCount > 0) {
+                        if (Objects.equals(ignoringClassPackage, matchedClassPackage)) {
+                            ignoringCount += 1;
+                        } else {
+                            ignoredStackTraceItemsConsumer.accept(ignoringClassPackage, ignoringCount);
+                            ignoringClassPackage = matchedClassPackage;
+                            ignoringCount = 1;
+                        }
+                    } else {
+                        ignoringClassPackage = matchedClassPackage;
+                        ignoringCount = 1;
+                    }
+                }
+            }
+            if (ignoringCount > 0) {
+                ignoredStackTraceItemsConsumer.accept(ignoringClassPackage, ignoringCount);
+            }
+        }
+    }
+
+    /**
+     * @since 3.0.0
+     */
+    @Nonnull
+    public String renderJsonToStringBlock(@Nullable String name, @Nullable Object object) {
+        if (object == null) {
+            return "null";
+        }
+        return renderJsonItem(name, object, 0, null);
+    }
+
+    /**
+     * @param key    Key of entry amongst the entries, or the index amongst the array.
+     * @param object Value of entry amongst the entries, or the item amongst the array.
+     * @return rendered string block ended with NEW_LINE.
+     */
+    @Nonnull
+    private String renderJsonItem(@Nullable String key, @Nullable Object object, int indentation, @Nullable String typeMark) {
+        StringBuilder subBlock = new StringBuilder();
+        if (indentation > 1) {
+            subBlock.append(" ".repeat(indentation - 2));
+            subBlock.append(typeMark).append(" ");
+        } else {
+            subBlock.append(" ".repeat(Math.max(0, indentation)));
+        }
+
+        if (key != null) {
+            subBlock.append(key).append(": ");
+        }
+        if (object instanceof JsonObject) {
+            subBlock.append("\n");
+            ((JsonObject) object).forEach(entry -> subBlock.append(
+                    renderJsonItem(entry.getKey(), entry.getValue(), indentation + 2, "+")
+            ));
+        } else if (object instanceof JsonArray) {
+            subBlock.append("\n");
+            for (int i = 0; i < ((JsonArray) object).size(); i++) {
+                subBlock.append(
+                        renderJsonItem(i + "", ((JsonArray) object).getValue(i), indentation + 2, "-")
+                );
+            }
+        } else {
+            subBlock.append(object).append("\n");
+        }
+        return subBlock.toString();
+    }
+}
