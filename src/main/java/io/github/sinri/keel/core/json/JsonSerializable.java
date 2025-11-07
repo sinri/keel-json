b69de02f46c5a383c00c45585c85a4680f331a84
Sinri Edogawa <e.joshua.s.e@gmail.com>
2025-11-07 19:18:30 +0800

    Refactor: Remove deprecated `toString` method from JsonSerializable interface
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

--- src/main/java/io/github/sinri/keel/core/json/JsonSerializable.java
+++ src/main/java/io/github/sinri/keel/core/json/JsonSerializable.java
@@ -15,18 +15,4 @@ public interface JsonSerializable {
      * @return the formatted JSON Object expression of this instance.
      */
     String toFormattedJsonExpression();
-
-    /**
-     * Should return same content with {@link JsonSerializable#toJsonExpression()}
-     *
-     * @return The JSON Object expression.
-     * @see JsonSerializable#toJsonExpression()
-     * @deprecated do not rely on this method to retrieve JSON expression, use
-     *         {@link JsonSerializable#toJsonExpression()} instead to avoid risks.
-     */
-    @Deprecated(since = "4.1.5")
-    @Override
-    String toString();
-
-
 }
